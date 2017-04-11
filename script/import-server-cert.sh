d23ffb602cba5f192d6795b9bf1f46be1e2a31e0
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-04-11 09:09:51 +0200

    chore(logging) log message to demonstrate asynchronous execution

--- script/import-server-cert.sh
+++ script/import-server-cert.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-filename=$1
 
-$JAVA_9_HOME/bin/keytool -import -v -trustcacerts -alias localhost-alias2 -file $filename -keystore ../lib/security/cacerts -keypass changeit -storepass changeit
+$JAVA_9_HOME/bin/keytool -import -v -trustcacerts -alias localhost-alias2 -file localhost.crt -keystore $JAVA_9_HOME/lib/security/cacerts -keypass changeit -storepass changeit
--- src/main/java/de/holisticon/jdk9showcase/http2client/ResponseAsyncExample.java
+++ src/main/java/de/holisticon/jdk9showcase/http2client/ResponseAsyncExample.java
@@ -8,7 +8,10 @@ import jdk.incubator.http.HttpResponse;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,44 +22,53 @@ import java.util.logging.Logger;
  */
 public class ResponseAsyncExample {
 
-    private static final Logger LOG = Logger
-            .getLogger(ResponseAsyncExample.class.getName());
-    private static final Version VERSION = Version.HTTP_2;
+  private static final Logger LOG = Logger
+      .getLogger(ResponseAsyncExample.class.getName());
+  private static final Version VERSION = Version.HTTP_2;
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        try {
-            LOG.log(Level.INFO,
-                    "##################################################");
-            LOG.log(Level.INFO, "");
-            ResponseAsyncExample example = new ResponseAsyncExample();
-            example.startRequest();
-            System.exit(0);
-        } catch (URISyntaxException e) {
-            LOG.log(Level.SEVERE, "URISyntaxException", e);
-        } catch (InterruptedException e) {
-            LOG.log(Level.SEVERE, "InterruptedException", e);
-        } catch (ExecutionException e) {
-            LOG.log(Level.SEVERE, "ExecutionException", e);
-        }
-
+    try {
+      LOG.log(Level.INFO,
+          "##################################################");
+      LOG.log(Level.INFO, "");
+      ResponseAsyncExample example = new ResponseAsyncExample();
+      example.startRequest();
+      System.exit(0);
+    } catch (URISyntaxException e) {
+      LOG.log(Level.SEVERE, "URISyntaxException", e);
+    } catch (InterruptedException | TimeoutException e) {
+      LOG.log(Level.SEVERE, "InterruptedException", e);
+    } catch (ExecutionException e) {
+      LOG.log(Level.SEVERE, "ExecutionException", e);
     }
 
-    private void startRequest() throws URISyntaxException,
-            InterruptedException, ExecutionException {
+  }
 
-        URI uri = new URI("https://localhost:8443/greeting?name=JavaLand");
-        String msg = "Sending request to: " + uri.toString();
-        LOG.log(Level.INFO, msg);
+  private void startRequest() throws URISyntaxException,
+      InterruptedException, ExecutionException, TimeoutException {
 
-        HttpClient client = ExampleUtils
-                .createHttpClient(VERSION);
-        HttpRequest request = ExampleUtils.createHttpRequest(uri, VERSION);
+    URI uri = new URI("https://localhost:8443/greeting?name=JavaLand");
+    String msg = "Sending request to: " + uri.toString();
+    LOG.log(Level.INFO, msg);
 
-        HttpResponse<String> response = client
-                .sendAsync(request, HttpResponse.BodyHandler.asString()).get();
+    HttpClient client = ExampleUtils
+        .createHttpClient(VERSION);
+    HttpRequest request = ExampleUtils.createHttpRequest(uri, VERSION);
 
-        ExampleUtils.printResponse(response);
-    }
+    CompletableFuture<HttpResponse<String>> completableFuture = client
+        .sendAsync(request, HttpResponse.BodyHandler.asString());
+    LOG.log(Level.SEVERE, "request created");
+
+    String result = completableFuture
+        .thenApplyAsync(stringHttpResponse -> stringHttpResponse.body())
+        .completeOnTimeout("a timout has occured", 300, TimeUnit.MILLISECONDS)
+        .get();
+
+    LOG.log(Level.INFO, result);
+
+    //ExampleUtils.printResponse(result);
+  }
+
 
 }
